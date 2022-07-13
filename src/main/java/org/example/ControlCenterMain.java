ac08b3e6b84e6489bb417190693adb35d29bcf1f
wubin28 <wubinben@icloud.com>
2022-07-14 00:57:01 +0800

    mars rover can print coordinates and direction

--- src/main/java/org/example/ControlCenterMain.java
+++ src/main/java/org/example/ControlCenterMain.java
@@ -3,12 +3,13 @@ package org.example;
 public class ControlCenterMain {
     public static void main(String[] args) {
         ControlCenter controlCenter = new ControlCenter();
-        MarsRover marsRover = new MarsRover();
+        MarsRover marsRover = new MarsRover(0, 0, "N");
         TurnLeftCommand turnLeftCommand = new TurnLeftCommand(marsRover);
         TurnRightCommand turnRightCommand = new TurnRightCommand(marsRover);
         MoveCommand moveCommand = new MoveCommand(marsRover);
 
-        controlCenter.storeCommands(turnLeftCommand, moveCommand, turnRightCommand);
+        controlCenter.storeCommands(moveCommand, turnLeftCommand, turnRightCommand, moveCommand, moveCommand);
         controlCenter.performStoredCommands();
+        System.out.println(marsRover.toString());
     }
 }
--- src/main/java/org/example/MarsRover.java
+++ src/main/java/org/example/MarsRover.java
@@ -1,15 +1,67 @@
 package org.example;
 
 public class MarsRover {
+    private int xCoordinate;
+    private int yCoordinate;
+    private String direction;
+
+    public MarsRover(int xCoordinate, int yCoordinate, String direction) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.direction = direction;
+    }
+
     public void turnLeft() {
-        System.out.println("L");
+        if ("N".equals(this.direction)) {
+            this.direction = "W";
+        }
+        if ("W".equals(this.direction)) {
+            this.direction = "S";
+        }
+        if ("S".equals(this.direction)) {
+            this.direction = "E";
+        }
+        if ("E".equals(this.direction)) {
+            this.direction = "N";
+        }
     }
 
     public void turnRight() {
-        System.out.println("R");
+        if ("N".equals(this.direction)) {
+            this.direction = "E";
+        }
+        if ("E".equals(this.direction)) {
+            this.direction = "S";
+        }
+        if ("S".equals(this.direction)) {
+            this.direction = "W";
+        }
+        if ("W".equals(this.direction)) {
+            this.direction = "N";
+        }
     }
 
     public void move() {
-        System.out.println("M");
+        if ("N".equals(this.direction)) {
+            this.yCoordinate++;
+        }
+        if ("E".equals(this.direction)) {
+            this.xCoordinate++;
+        }
+        if ("S".equals(this.direction)) {
+            this.yCoordinate--;
+        }
+        if ("W".equals(this.direction)) {
+            this.xCoordinate--;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MarsRover{" +
+                "xCoordinate=" + xCoordinate +
+                ", yCoordinate=" + yCoordinate +
+                ", direction='" + direction + '\'' +
+                '}';
     }
 }
