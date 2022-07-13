a40d3db41a1e4c7a7980f6c9e15d381c91443659
wubin28 <wubinben@icloud.com>
2022-07-14 01:56:38 +0800

    test: add should_move_to_0_3_N_when_MLRMM_from_0_0_N()

--- src/test/java/org/example/ControlCenterTest.java
+++ src/test/java/org/example/ControlCenterTest.java
@@ -209,4 +209,23 @@ class ControlCenterTest {
         assertEquals(0, marsRover.getYCoordinate());
         assertEquals("N", marsRover.getDirection());
     }
+
+    @Test
+    void should_move_to_0_3_N_when_MLRMM_from_0_0_N() {
+        // given
+        ControlCenter controlCenter = new ControlCenter();
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        TurnLeftCommand turnLeftCommand = new TurnLeftCommand(marsRover);
+        TurnRightCommand turnRightCommand = new TurnRightCommand(marsRover);
+        MoveCommand moveCommand = new MoveCommand(marsRover);
+        controlCenter.storeCommands(moveCommand, turnLeftCommand, turnRightCommand, moveCommand, moveCommand);
+
+        // when
+        controlCenter.performStoredCommands();
+
+        // then
+        assertEquals(0, marsRover.getXCoordinate());
+        assertEquals(3, marsRover.getYCoordinate());
+        assertEquals("N", marsRover.getDirection());
+    }
 }
