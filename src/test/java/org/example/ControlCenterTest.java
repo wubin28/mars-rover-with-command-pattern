08af411862358f1a3231018295ef66c647e32d48
wubin28 <wubinben@icloud.com>
2022-07-14 01:44:31 +0800

    test: should_move_to_1_0_E_when_move_from_0_0_E()

--- src/test/java/org/example/ControlCenterTest.java
+++ src/test/java/org/example/ControlCenterTest.java
@@ -107,4 +107,21 @@ class ControlCenterTest {
         assertEquals(0, marsRover.getYCoordinate());
         assertEquals("W", marsRover.getDirection());
     }
+
+    @Test
+    void should_move_to_1_0_E_when_move_from_0_0_E() {
+        // given
+        ControlCenter controlCenter = new ControlCenter();
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+        MoveCommand moveCommand = new MoveCommand(marsRover);
+        controlCenter.storeCommands(moveCommand);
+
+        // when
+        controlCenter.performStoredCommands();
+
+        // then
+        assertEquals(1, marsRover.getXCoordinate());
+        assertEquals(0, marsRover.getYCoordinate());
+        assertEquals("E", marsRover.getDirection());
+    }
 }
