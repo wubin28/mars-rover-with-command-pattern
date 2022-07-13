d0d919394b3ff781a0ef926fcc1f07d9552aa748
wubin28 <wubinben@icloud.com>
2022-07-14 01:46:14 +0800

    test: add should_move_to_0_0_N_when_turn_left_from_0_0_E()

--- src/test/java/org/example/ControlCenterTest.java
+++ src/test/java/org/example/ControlCenterTest.java
@@ -124,4 +124,21 @@ class ControlCenterTest {
         assertEquals(0, marsRover.getYCoordinate());
         assertEquals("E", marsRover.getDirection());
     }
+
+    @Test
+    void should_move_to_0_0_N_when_turn_left_from_0_0_E() {
+        // given
+        ControlCenter controlCenter = new ControlCenter();
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+        TurnLeftCommand turnLeftCommand = new TurnLeftCommand(marsRover);
+        controlCenter.storeCommands(turnLeftCommand);
+
+        // when
+        controlCenter.performStoredCommands();
+
+        // then
+        assertEquals(0, marsRover.getXCoordinate());
+        assertEquals(0, marsRover.getYCoordinate());
+        assertEquals("N", marsRover.getDirection());
+    }
 }
