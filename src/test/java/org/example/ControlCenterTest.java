dc555b56c4284bb2e2611c1758f108f0d5fb70e4
wubin28 <wubinben@icloud.com>
2022-07-14 01:42:25 +0800

    test: should_move_to_0_0_W_when_turn_right_from_0_0_S()

--- src/test/java/org/example/ControlCenterTest.java
+++ src/test/java/org/example/ControlCenterTest.java
@@ -90,4 +90,21 @@ class ControlCenterTest {
         assertEquals(0, marsRover.getYCoordinate());
         assertEquals("E", marsRover.getDirection());
     }
+
+    @Test
+    void should_move_to_0_0_W_when_turn_right_from_0_0_S() {
+        // given
+        ControlCenter controlCenter = new ControlCenter();
+        MarsRover marsRover = new MarsRover(0, 0, "S");
+        TurnRightCommand turnRightCommand = new TurnRightCommand(marsRover);
+        controlCenter.storeCommands(turnRightCommand);
+
+        // when
+        controlCenter.performStoredCommands();
+
+        // then
+        assertEquals(0, marsRover.getXCoordinate());
+        assertEquals(0, marsRover.getYCoordinate());
+        assertEquals("W", marsRover.getDirection());
+    }
 }
