c374307776aa345db3b80473130e4e4a1ecbfba8
wubin28 <wubinben@icloud.com>
2022-07-14 00:02:30 +0800

    add a green test

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+    @Test
+    void should() {
+        assertEquals(1, 1);
+    }
+}
